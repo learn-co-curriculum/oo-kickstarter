be4d9c804fb9ae5112c8c2f77a0e315bb16d28ca
Logan Hasson <logan.hasson@gmail.com>
2014-02-12 11:54:55 -0500

    Complete solution

--- backer.rb
+++ backer.rb
@@ -1,8 +1,14 @@
 class Backer
-  attr_reader :name
+  attr_reader :name, :backed_projects
 
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
   end
 
 end
