085b2bdf201ef56a4a78d8ee29b1f33e1eb4cb28
maxwellbenton <maxwellbenton@gmail.com>
2019-02-04 17:17:51 -0500

    updates solution

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
+end
